3424dfe08bc63a97e26fd2a04b5c79da6f58e18d
mgardner <mgardner@sumtotalsystems.com>
2015-02-19 16:29:21 -0500

    moving back the jsexamples.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -133,8 +133,7 @@ module.exports = function (grunt) {
         src: [
           'docs/assets/js/_vendor/holder.js',
           'docs/assets/js/_vendor/ZeroClipboard.min.js',
-          'docs/assets/js/_src/application.js',
-          'docs/assets/js/_src/jsExamples.js'
+          'docs/assets/js/_src/application.js'
         ],
         dest: 'docs/assets/js/docs.min.js'
       }
