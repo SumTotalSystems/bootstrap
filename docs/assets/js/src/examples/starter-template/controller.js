37385a27c0a8b88615a408014aa21be383bbc2e0
mgardner <mgardner@sumtotalsystems.com>
2015-03-30 15:36:53 -0400

    data to the side panel in the example

--- docs/assets/js/src/examples/starter-template/controller.js
+++ docs/assets/js/src/examples/starter-template/controller.js
@@ -1,4 +1,15 @@
 angular.module('Examples')
-  .controller('starterTemplateController', ['$scope', function ($scope) {
-    $scope.user = faker.helpers.userCard();
+  .controller('starterTemplateController', ['$scope', 'ExamplesData', function ($scope, ExamplesData) {
+
+    $scope.user = {};
+    $scope.panels = {};
+
+    ExamplesData.userInfo().then(function (userInfo) {
+      $scope.user = userInfo;
+    });
+
+    ExamplesData.sidePanelData().then(function (sidePanelData) {
+      $scope.panels = sidePanelData;
+    });
+
 }]);
